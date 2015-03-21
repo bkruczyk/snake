a5dfb7f202cb3b939a6d77428faba5d19d946f68
Bartłomiej Kruczyk <bartlomiej.kruczyk@gmail.com>
2015-03-21 01:05:10 +0100

    eat apples

--- app.js
+++ app.js
@@ -17,8 +17,35 @@ var Inputter = function() {
     self = this;
     
     window.onkeydown = function(e) {
-        self.lastKey = e.keyCode;
+        self.setLastKey(e.keyCode);
     };
+};
+
+Inputter.prototype = {
+    setLastKey: function(key) {
+        switch (key) {
+        case KEY.UP:
+            if (this.lastKey !== KEY.DOWN) {
+                this.lastKey = KEY.UP;
+            }
+            break;
+        case KEY.DOWN:
+            if (this.lastKey !== KEY.UP) {
+                this.lastKey = KEY.DOWN;
+            }
+            break;
+        case KEY.RIGHT:
+            if (this.lastKey !== KEY.LEFT) {
+                this.lastKey = KEY.RIGHT;
+            }
+            break;
+        case KEY.LEFT:
+            if (this.lastKey !== KEY.RIGHT) {
+                this.lastKey = KEY.LEFT;
+            }
+            break;
+        }
+    }
 };
 
 var Board = function(blockSize, width, height) {
@@ -29,26 +56,46 @@ var Board = function(blockSize, width, height) {
 
 var Snake = function(segments) {
     this.segments = segments;
+    this.head = { x: segments[0].x, y: segments[0].y };
     this.color = "#000";
 };
 
 Snake.prototype = {
     move: function(direction) {
-        var head = { x: this.segments[0].x, y: this.segments[0].y };
         var trimmed = this.segments.slice(0, -1);
         
         switch (direction) {
         case KEY.UP:
-            return new Snake([{ x: head.x, y: head.y - 1 }].concat(trimmed));
+            return new Snake(
+                [{ x: this.head.x, y: this.head.y - 1 }].concat(trimmed)
+            );
         case KEY.DOWN:
-            return new Snake([{ x: head.x, y: head.y + 1 }].concat(trimmed));
+            return new Snake(
+                [{ x: this.head.x, y: this.head.y + 1 }].concat(trimmed)
+            );
         case KEY.LEFT:
-            return new Snake([{ x: head.x - 1, y: head.y }].concat(trimmed));
+            return new Snake(
+                [{ x: this.head.x - 1, y: this.head.y }].concat(trimmed)
+            );
         case KEY.RIGHT:
-            return new Snake([{ x: head.x + 1, y: head.y }].concat(trimmed));
+            return new Snake(
+                [{ x: this.head.x + 1, y: this.head.y }].concat(trimmed)
+            );
         }
 
         return new Snake(this.segments);
+    },
+    eat: function(apple) {
+        return new Snake([{ x: apple.x, y: apple.y }].concat(this.segments));
+    },
+    isOutOfBoard: function(board) {
+        return this.head.x < 0
+            || this.head.y < 0
+            || this.head.x > board.width - 1
+            || this.head.y > board.height - 1;
+    },
+    collidesWithItself: function() {
+        return collidesWith(this.head, this.segments.slice(1));
     }
 };
 
@@ -56,12 +103,15 @@ var Game = function(canvasId) {
     this.WIDTH = 200;
     this.HEIGHT = 200;
     this.BLOCK_SIZE = 10;
-    this.MOVE_TIME = 50;
+    this.MOVE_TIME = 10;
 
     this.timeToMove = this.MOVE_TIME;
     this.inputter = new Inputter();
     this.board = new Board(this.BLOCK_SIZE, this.WIDTH, this.HEIGHT);
-    this.snake = new Snake([{ x: this.board.width / 2, y: this.board.height / 2 }]);
+    this.snake = new Snake([
+        { x: this.board.width / 2, y: this.board.height / 2 },
+        { x: this.board.width / 2 + 1, y: this.board.height / 2}
+    ]);
     this.apple = this.placeApple(this.snake, this.board);
     
     var canvas = document.getElementById(canvasId);
@@ -93,11 +143,20 @@ Game.prototype = {
             return;
         }
 
-        this.timeToMove = this.MOVE_TIME;;
-        
+        this.timeToMove = this.MOVE_TIME;
+
         var moved = this.snake.move(this.inputter.lastKey);
 
-        this.snake = moved;
+        if (collidesWith(moved.head, [this.apple])) {
+            var newSnake = this.snake.eat(this.apple);
+
+            this.apple = this.placeApple(newSnake, this.board);
+            this.snake = newSnake;
+        } else if (moved.isOutOfBoard(this.board) || moved.collidesWithItself()) {
+            this.snake.color = "#0000FF";
+        } else {
+            this.snake = moved;
+        }
     },
     draw: function(drawingContext) {
         console.log("loop");
@@ -126,7 +185,9 @@ Game.prototype = {
                 x: Math.floor(Math.random() * 10),
                 y: Math.floor(Math.random() * 10)
             };
-        } while (apple.x > board.width || apple.y > board.height || collidesWith(apple, snake.segments));
+        } while (apple.x > board.width
+                 || apple.y > board.height
+                 || collidesWith(apple, snake.segments));
 
         return apple;
     }
